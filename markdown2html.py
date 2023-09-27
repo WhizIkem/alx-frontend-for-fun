c672d9280d142ead8776b15d7447ebfff06bc728
whizikem <wisdomikem@gmail.com>
2023-09-27 13:01:06 +0000

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -3,9 +3,30 @@
 Script markdown2html.py that takes an argument 2 strings:
     first argument is the name of the markdown file
     second argument is the output file name
+Parse Markdown headings and return corresponding HTML
 """
 import sys
 import os
+import re
+
+
+def parse_headings(line):
+    # Check for headings
+    match = re.match(r'(#+) (.+)', line)
+    if match:
+        level = len(match.group(1))
+        content = match.group(2)
+        return f"<h{level}>{content}</h{level}>"
+    return line
+
+
+def markdown_to_html(filename):
+    # Convert markdown content to HTML
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    html_lines = [parse_headings(line.strip()) for line in lines]
+    return '\n'.join(html_lines)
 
 
 def main():
@@ -16,10 +37,17 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
     # Check if the markdown file doesn't exist
     if not os.path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+
+    html_content = markdown_to_html(input_file)
+
+    with open(output_file, 'w') as f:
+        f.write(html_content)
 
     # If all checks pass
     sys.exit(0)
